50073776f46465e33bbf93d8895d539aedcb644d
mansivakte <mansi25vv@gmail.com>
2022-12-20 18:09:54 +0530

    initial commit

---
+++ src/Components/Common/StackNavbar.js
@@ -0,0 +1,108 @@
+import React from "react";
+import StackImg from "../Assets/stackoverflow.png";
+
+function StackNavbar() {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            <img
+              src={StackImg}
+              width="45px"
+              height="35px"
+              class="d-inline-block align-text-top"
+            ></img>
+            Stackoverflow
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  About
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Products
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  For Teams
+                </a>
+              </li>
+            </ul>
+            {/* 
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Dropdown
+                </a>
+                <ul className="dropdown-menu">
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Action
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Another action
+                    </a>
+                  </li>
+                  <li>
+                    <hr className="dropdown-divider" />
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Something else here
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link disabled">Disabled</a>
+              </li>
+            </ul> */}
+            <form className="d-flex" role="search">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              {/* <button className="btn btn-outline-success" type="submit">
+                Search
+              </button> */}
+            </form>
+            <button type="button" class="btn btn-outline-primary">
+              Log in
+            </button>
+            <button type="button" class="btn btn-outline-primary">
+              Sign up
+            </button>
+          </div>
+        </div>
+      </nav>
+      ;
+    </div>
+  );
+}
+
+export default StackNavbar;
